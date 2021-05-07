0ea6e0e3766f1935e52e5d604a83ebc9f9fce9a7
gb85150 <geoffrey.bousseau@gmail.com>
2021-05-07 18:16:32 +0200

    Mini Patch NSI 07/05/2021 18:16

--- loader.js
+++ loader.js
@@ -2,7 +2,6 @@ function load() {
   alert("click")
   choice = prompt("Click ? (Y/N)")
   if(choice == "Y") {
-    alert("oui")
     url = document.getElementById("url_input")
     window.open(url, '_blank')
   }
