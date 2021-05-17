3dbdd546cd4f784f2f04454e8720cbcce57d93e1
xts.en <72768267+xtsen@users.noreply.github.com>
2021-05-17 09:54:07 +0200

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,11 +1,7 @@
 function load() {
-  alert("click")
-  choice = prompt("Click ? (Y/N)")
-  if(choice == "Y") {
-    url = document.getElementById("url_input")
-    window.open(url, '_blank')
-  }
-  else {
-    alert("non")
-  }
+    document.getElementById("formInput").style.display="none";
+    document.getElementById("Answer").style.display="block";
+    document.getElementById("Answer").style.fontSize="140%";
+    Website = document.getElementById("url_input").value;
+    document.getElementById("Website").innerText=Website;
 }
