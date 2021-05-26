b4dfb9bdeac74fd75a8990e4a2d85840d8903d49
gb85150 <geoffrey.bousseau@gmail.com>
2021-05-26 11:21:45 +0200

    Little tweaks + travail du legal.html

--- loader.js
+++ loader.js
@@ -2,11 +2,8 @@ function load() {
     document.getElementById("formInput").style.display="none";
     document.getElementById("Answer").style.display="block";
     document.getElementById("Answer").style.fontSize="140%";
-    Website = document.getElementById("url_input").value;
+    let Website = document.getElementById("url_input").value;
     document.getElementById("Website").innerText=Website;
-    url = "https://" + Website
-
+    url = "http://" + Website
     setTimeout(function(){ window.location.replace(url); }, 2000);
-    
-    
 }
