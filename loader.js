0a640165520393a30f13d8d3f02beba0cfe28064
gb85150 <geoffrey.bousseau@gmail.com>
2021-06-07 02:08:08 +0200

    Ajout d'un lien Désactivation controlle input TWeak CSS

--- loader.js
+++ loader.js
@@ -4,15 +4,17 @@ function load() {
     document.getElementById("Answer").style.fontSize="140%";
     let Website = document.getElementById("url_input").value;
     document.getElementById("Website").innerText=Website;
-    if(url != "") {
-      compose_url();
-      setTimeout(function(){ window.location.replace(url); }, 2000);
-    }
-    else {
-      document.getElementById("Answer").innerText="Error, Please provide clean url (BAD_URL)"
-      alert("Error, Please provide clean url (BAD_URL)")
-    }
-  function compose_url() {
-    url = "http://" + Website;
-  }
+    url = "http://" + Website
+    setTimeout(function(){ window.location.replace(url); }, 2000);
+//    if(url != "") {
+//      compose_url();
+//      setTimeout(function(){ window.location.replace(url); }, 2000);
+//    }
+//    else {
+//      document.getElementById("Answer").innerText="Error, Please provide clean url (BAD_URL)"
+//      alert("Error, Please provide clean url (BAD_URL)")
+//    }
+//  function compose_url() {
+//    url = "http://" + Website;
+//  }
 }
